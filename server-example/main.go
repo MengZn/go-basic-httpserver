8c5480bbb6d411e51a64c01f6cbf2f0cee2c95ff
MengZn <adnt587@gmail.com>
2019-10-02 09:39:30 +0800

    add http handler for checkout the book

--- server-example/main.go
+++ server-example/main.go
@@ -14,6 +14,11 @@ type RegResponse struct {
 	Info string
 }
 
+type CheckInRequest struct {
+	Book string
+}
+type CheckInResponse RegResponse
+
 func regHandler(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	switch r.Method {
@@ -36,7 +41,31 @@ func regHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func checkOutHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	switch r.Method {
+	case http.MethodPost:
+		var t CheckInRequest
+		if err := json.NewDecoder(r.Body).Decode(&t); err != nil {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		detail := "You got a " + t.Book + " book"
+		info := CheckInResponse{Info: detail}
+		if resp, err := json.Marshal(info); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		} else {
+			w.Write(resp)
+		}
+	default:
+		w.WriteHeader(http.StatusMethodNotAllowed)
+	}
+}
+
 func main() {
 	http.HandleFunc("/reg", regHandler)
+	http.HandleFunc("/checkout", checkOutHandler)
+
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
